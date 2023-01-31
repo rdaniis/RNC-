b5ace69c8a587dec88d941c58ed12ef5f60adef5
Daniel Rodriguez <124081286+rdaniis@users.noreply.github.com>
2023-01-31 16:18:24 -0600

    Change2

---
+++ capacitor.config.ts
@@ -0,0 +1,10 @@
+import { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'io.ionic.starter',
+  appName: 'rnc',
+  webDir: 'www',
+  bundledWebRuntime: false
+};
+
+export default config;
